8270b71f9f751623792d76ab1ad25a0aeabc6b80
Gabriel Giovanini <gabrielgio@localhost.localdomain>
2018-04-04 22:06:37 -0300

    First Step To xUnit

---
+++ main.py
@@ -0,0 +1,32 @@
+class TestCase:
+
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+
+class WasRun(TestCase):
+
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
+    def run(self):
+        self.test_method()
+
+    def test_method(self):
+        self.wasRun = 1
+
+
+class TestCaseTest(TestCase):
+    def test_running(self):
+        test = WasRun("test_method")
+        assert (not test.wasRun)
+        test.run()
+        assert (test.wasRun)
+
+
+TestCaseTest("test_running").run()
