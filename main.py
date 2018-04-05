a6772989986cfa3f99bd3987bf8cb34af4b4b0d1
Gabriel Giovanini <gabrielgio@localhost.localdomain>
2018-04-04 22:45:42 -0300

    Set The Table

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-class TestCase:
+class TestCase(object):
 
     def __init__(self, name):
         self.name = name
 
+    def set_up(self):
+        pass
+
     def run(self):
+        self.set_up()
         method = getattr(self, self.name)
         method()
 
@@ -11,22 +15,29 @@ class TestCase:
 class WasRun(TestCase):
 
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
-
-    def run(self):
-        self.test_method()
 
     def test_method(self):
         self.wasRun = 1
 
+    def set_up(self):
+        self.wasRun = None
+        self.wasSetup = 1
+
 
 class TestCaseTest(TestCase):
+
+    def set_up(self):
+        self.test = WasRun("test_method")
+
     def test_running(self):
-        test = WasRun("test_method")
-        assert (not test.wasRun)
-        test.run()
-        assert (test.wasRun)
+        self.test.run()
+        assert self.test.wasRun
+
+    def test_set_up(self):
+        self.test.run()
+        assert self.test.wasSetup
 
 
+TestCaseTest("test_set_up").run()
 TestCaseTest("test_running").run()
